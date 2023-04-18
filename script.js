e7253391eb6feb5757cdc400ba6288872328cf99
TAMS-TECHNOLOGY <dummytwiz@gmail.com>
2023-04-18 11:56:23 +0100

    did corrections

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function toggleNavMenu() {
   if (menu.style.display === "none") {
     menu.style.display = "block";
   } else if (menu.style.display == "flex") {
-    menu.style.display = "flex";
+    menu.style.display = "none";
   } else {
     menu.style.display = "none";
   }
